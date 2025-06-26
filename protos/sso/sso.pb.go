534fdc2e08b9f681953bce47fd5519454f2d02e2
vyantik <akarizidu2@gmail.com>
2025-06-26 03:26:20 +0300

    Refactor RegisterRequest in protobuf definitions to include a username field. Updated related generated Go files to reflect this change, ensuring proper handling of username and password fields.

--- protos/sso/sso.pb.go
+++ protos/sso/sso.pb.go
@@ -24,7 +24,8 @@ const (
 type RegisterRequest struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
 	Email         string                 `protobuf:"bytes,1,opt,name=email,proto3" json:"email,omitempty"`
-	Password      string                 `protobuf:"bytes,2,opt,name=password,proto3" json:"password,omitempty"`
+	Username      string                 `protobuf:"bytes,2,opt,name=username,proto3" json:"username,omitempty"`
+	Password      string                 `protobuf:"bytes,3,opt,name=password,proto3" json:"password,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -62,6 +63,13 @@ func (*RegisterRequest) Descriptor() ([]byte, []int) {
 func (x *RegisterRequest) GetEmail() string {
 	if x != nil {
 		return x.Email
+	}
+	return ""
+}
+
+func (x *RegisterRequest) GetUsername() string {
+	if x != nil {
+		return x.Username
 	}
 	return ""
 }
@@ -313,10 +321,11 @@ var File_protos_sso_sso_proto protoreflect.FileDescriptor
 
 const file_protos_sso_sso_proto_rawDesc = "" +
 	"\n" +
-	"\x14protos/sso/sso.proto\x12\x04auth\"C\n" +
+	"\x14protos/sso/sso.proto\x12\x04auth\"_\n" +
 	"\x0fRegisterRequest\x12\x14\n" +
 	"\x05email\x18\x01 \x01(\tR\x05email\x12\x1a\n" +
-	"\bpassword\x18\x02 \x01(\tR\bpassword\"\"\n" +
+	"\busername\x18\x02 \x01(\tR\busername\x12\x1a\n" +
+	"\bpassword\x18\x03 \x01(\tR\bpassword\"\"\n" +
 	"\x10RegisterResponse\x12\x0e\n" +
 	"\x02id\x18\x01 \x01(\x03R\x02id\"W\n" +
 	"\fLoginRequest\x12\x14\n" +
